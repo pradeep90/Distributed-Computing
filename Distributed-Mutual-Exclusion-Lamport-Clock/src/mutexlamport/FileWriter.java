9a0b3b90105ff52095eaf3763bc7e6ab87bc63e4
S Pradeep Kumar <gohanpra@gmail.com>
2012-08-30 01:43:46 +0530

    Added code in FileWriter to appendToFile and generate random number and wait for half a sec.

--- Distributed-Mutual-Exclusion-Lamport-Clock/src/mutexlamport/FileWriter.java
+++ Distributed-Mutual-Exclusion-Lamport-Clock/src/mutexlamport/FileWriter.java
@@ -3,10 +3,46 @@ package mutexlamport;
 import java.net.*;
 import java.io.*;
 import java.lang.*;
+import java.util.Random;
 
 public class FileWriter {
 
+    String filename;
+
     public static void main(String argv[]) {
-        System.out.println ("Yo, boyz!");
+        Random randomGenerator = new Random ();
+        String message = "Yo, boyz!";
+        for (int i = 0; i < 10; i++){
+            int randomInt = randomGenerator.nextInt (1000);
+            String randomMessage = message + " " + randomInt;
+            System.out.println (randomMessage);
+            FileWriter writer = new FileWriter ("output.txt");
+            writer.appendToFile (randomMessage + "\n");
+            try {
+                Thread.sleep(500);
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    FileWriter (String givenFilename){
+        filename = givenFilename;
+    }
+
+    // TODO: Should a new java.io.FileWriter instance be
+    // created each time or can we reuse one instance?
+    public void appendToFile (String message){
+        try{
+            // Create file 
+            java.io.FileWriter fstream = new java.io.FileWriter(filename, true);
+            BufferedWriter out = new BufferedWriter(fstream);
+            out.write(message);
+
+            //Close the output stream
+            out.close();
+        }catch (Exception e){//Catch exception if any
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
     }
 }
